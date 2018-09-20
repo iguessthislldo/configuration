58da89e90b685d23a5d5a85eb3263c356ee4f7fa
Fred Hornsey <fred@hornsey.us>
2018-09-20 11:16:56 -0500

    rc.zsh: Remove perl stuff

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -87,9 +87,3 @@ setopt histignorespace
 
 # Profile
 source $CONFIG/profile
-
-PATH="/home/fred/perl5/bin${PATH:+:${PATH}}"; export PATH;
-PERL5LIB="/home/fred/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
-PERL_LOCAL_LIB_ROOT="/home/fred/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
-PERL_MB_OPT="--install_base \"/home/fred/perl5\""; export PERL_MB_OPT;
-PERL_MM_OPT="INSTALL_BASE=/home/fred/perl5"; export PERL_MM_OPT;
