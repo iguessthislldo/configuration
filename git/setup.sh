50cc56c61e4792d544289aff3789de63dac07ef8
Fred Hornsey <fred@hornsey.us>
2021-06-13 16:53:42 -0500

    Auto Sign Commits

--- git/setup.sh
+++ git/setup.sh
@@ -8,6 +8,8 @@ git config --global --unset user.name
 git config --global --unset user.email
 git config --global --unset user.signingkey
 
+git config --global commit.gpgsign true
+
 source "$CONFIG/sh/igtd_sh_loader.sh"
 igtd_sh_loader "$CONFIG/git/identities.d"
 unset -f igtd_sh_loader
