c40e668f8e9a495713f3c93653e202e38cd084ba
Fred Hornsey <fred@hornsey.us>
2021-08-02 02:39:42 -0500

    new: OCI TAO

--- new/ace-tao/clone_ace_tao.sh
+++ new/ace-tao/clone_ace_tao.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 cd "%dest%"
-git clone git@github.com:iguessthislldo/ACE_TAO.git
-cd "ACE_TAO"
+if %oci_tao%
+then
+    git clone hornseyf@git.ociweb.com:/git/ocitao
+    cd "ocitao"
+else
+    git clone git@github.com:iguessthislldo/ACE_TAO.git
+    cd "ACE_TAO"
+fi
 git remote add upstream git@github.com:DOCGroup/ACE_TAO.git
 gitid use work
--- new/ace-tao/setenv.sh
+++ new/ace-tao/setenv.sh
@@ -1,6 +1,12 @@
 workspace="%dest%"
-export ACE_ROOT=${workspace}/ACE_TAO/ACE
-export TAO_ROOT=${workspace}/ACE_TAO/TAO
+if %oci_tao%
+then
+    ace_tao_dir_name=ocitao
+else
+    ace_tao_dir_name=ACE_TAO
+fi
+export ACE_ROOT=${workspace}/${ace_tao_dir_name}/ACE
+export TAO_ROOT=${workspace}/${ace_tao_dir_name}/TAO
 export MPC_ROOT="%mpc%"
 
 export LD_LIBRARY_PATH=$ACE_ROOT/lib:${LD_LIBRARY_PATH}
