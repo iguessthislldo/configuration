bceb019ac6748c67bbe00da93fcfba6f633e31ea
Fred Hornsey <fred@hornsey.us>
2024-06-04 15:55:03 -0500

    Check for fc-cache

--- fonts/.install_this.sh
+++ fonts/.install_this.sh
@@ -1,4 +1,9 @@
-InstallDir .local/share
-InstallLink .local/share/fonts
-InstallRun fc-cache -f -v .
-ExportFiles "-name '*.otf' -o -name '*.ttf'"
+if command -v fc-cache &> /dev/null
+then
+    InstallDir .local/share
+    InstallLink .local/share/fonts
+    InstallRun fc-cache -f -v .
+    ExportFiles "-name '*.otf' -o -name '*.ttf'"
+else
+    echo "fc-cache command not available"
+fi
