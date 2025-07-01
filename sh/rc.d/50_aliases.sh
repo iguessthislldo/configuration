b857dfda2546f0c8469a9911e7760d8938749bbb
Fred Hornsey <fred@hornsey.us>
2025-06-30 23:48:52 -0500

    Tweak Custom Aliases

--- sh/rc.d/50_aliases.sh
+++ sh/rc.d/50_aliases.sh
@@ -1,10 +1,29 @@
-alias o="xdg-open"
-alias p="ptpython3"
-alias P="$EDITOR -R -"
-alias lP="l|P"
-alias llP="ll|P"
+if command -v xdg-open &> /dev/null
+then
+    alias o="xdg-open"
+fi
+
+if ! command -v py &> /dev/null
+then
+    if command -v ptpython3 &> /dev/null
+    then
+        alias py="ptpython3"
+    elif command -v ipython &> /dev/null
+    then
+        alias py="ipython"
+    elif command -v python3 &> /dev/null
+    then
+        alias py="python3"
+    elif command -v python &> /dev/null
+    then
+        alias py="python"
+    fi
+fi
+
 alias l="ls --color=always --classify --group-directories-first"
 alias cl="clear && l"
 alias la="l --almost-all"
 alias ll="la -l --si"
-alias giff="git diff --no-index"
+
+alias gdiff="git diff --no-index"
+alias giff="gdiff"
