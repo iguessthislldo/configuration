0cac50a843c04e0aad44b809ad1967b771dffad0
Fred Hornsey <fred@hornsey.us>
2025-07-26 02:41:30 -0500

    Copy OMZ Compaudit Code To Run Early

--- sh/environment.d/02-zsh-specific.zsh
+++ sh/environment.d/02-zsh-specific.zsh
@@ -2,3 +2,16 @@
 # by default on Debian based systems.
 # https://github.com/flox/flox/issues/2747
 fpath=($fpath /usr/share/zsh/site-functions/)
+
+function _igtd_warn_about_compinit {
+  local -aU insecure_dirs
+  insecure_dirs=( ${(f@):-"$(compaudit 2>/dev/null)"} )
+
+  # If no such directories exist, get us out of here.
+  [[ -z "${insecure_dirs}" ]] && return
+
+  # List ownership and permissions of all insecure directories.
+  print "Insecure completion-dependent directories detected:"
+  ls -ld "${(@)insecure_dirs}"
+}
+_igtd_warn_about_compinit
