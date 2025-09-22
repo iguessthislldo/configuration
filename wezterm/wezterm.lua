0bd71950c72238e1e102cef158b45c1126faab93
Fred Hornsey <fred@hornsey.us>
2025-09-22 13:22:19 -0500

    Fix Issues with WezTerm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,17 +4,17 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Decaf (base16)'
 
-if os.getenv("IGTD_MSYS2") == "true" then
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   config.front_end = "WebGpu"
   config.launch_menu = {
-    {
-      label = "Command Prompt",
-      args = { "cmd.exe" },
-    },
     {
       label = "UCRT64 / MSYS2 zsh",
       args = { "C:\\msys64\\msys2_shell.cmd", "-defterm", "-here", "-no-start", "-ucrt64", "-shell", "zsh", "-l" },
       cwd = "C:\\msys64\\home\\" ..  os.getenv("USERNAME"),
+    },
+    {
+      label = "Command Prompt",
+      args = { "cmd.exe" },
     },
   }
 else
