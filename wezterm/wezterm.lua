1f09b72a732f0ca62dd31eed46e9b0b37d1e8a87
Fred Hornsey <fred@hornsey.us>
2025-09-22 13:28:26 -0500

    Make Zsh The Default on WezTerm on Windows

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,11 +6,15 @@ config.color_scheme = 'Decaf (base16)'
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   config.front_end = "WebGpu"
+  local msys2_zsh = { "C:\\msys64\\msys2_shell.cmd", "-defterm", "-here", "-no-start", "-ucrt64", "-shell", "zsh", "-l" }
+  local msys2_home = "C:\\msys64\\home\\" ..  os.getenv("USERNAME")
+  config.default_prog = msys2_zsh
+  config.default_cwd = msys2_home
   config.launch_menu = {
     {
       label = "UCRT64 / MSYS2 zsh",
-      args = { "C:\\msys64\\msys2_shell.cmd", "-defterm", "-here", "-no-start", "-ucrt64", "-shell", "zsh", "-l" },
-      cwd = "C:\\msys64\\home\\" ..  os.getenv("USERNAME"),
+      args = msys2_zsh,
+      cwd = msys2_home,
     },
     {
       label = "Command Prompt",
